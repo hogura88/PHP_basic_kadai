ffb514e9ac045a0ad4ab7861f611c48f66946be7
hogura88 <hogura88@gmail.com>
2024-05-28 16:54:24 +0900

    復習

---
+++ index2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>この文章はHTMLで出力しています。</p>
+     <p>
+        <?php
+        echo'この文章はPHPで出力しています。';
+
+        echo 45 + 18;
+        echo '<br>';
+        echo 30 - 12;
+        echo '<br>';
+        echo 15 * 6;
+        echo '<br>';
+        echo 30 / 3;
+        echo '<br>';
+        echo 45 % 8;
+        ?>
+    
+     </p>
+ </body>
+ 
+ </html>
