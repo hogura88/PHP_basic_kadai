7530ee06835e60e9d682c101372b24c659801d38
hogura88 <hogura88@gmail.com>
2024-05-29 05:57:06 +0900

    復習

--- index2.php
+++ index2.php
@@ -21,6 +21,20 @@
         echo 30 / 3;
         echo '<br>';
         echo 45 % 8;
+        echo '<br>';
+
+        $user_name = '侍太郎';
+        echo $user_name;
+        echo '<br>';
+        $user_name = '侍花子';
+        echo $user_name;
+
+        $number = 3;
+        echo $number - 1.2;
+        echo '<br>';
+        $my_name = '侍一郎';
+        echo '私の名前は' . $my_name . 'です。';
+
         ?>
     
      </p>
