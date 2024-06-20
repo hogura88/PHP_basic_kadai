cce3d4767a0ecb52395471ed4cb312ac36a79290
hogura88 <hogura88@gmail.com>
2024-06-20 15:54:06 +0900

    review クラス

--- index2.php
+++ index2.php
@@ -302,6 +302,28 @@
         print_r($user);
         ?>
     </p>
+    <p>
+        <?php
+        class Product {
+            private $name;
+
+            public function set_name(string $name) {
+                $this->name = $name;
+            }
+            public function show_name() {
+                echo $this->name . '<br>';
+            }
+        }
+        $coffee = new Product();
+        $coffee->set_name('コーヒー');
+        $coffee->show_name();
+
+
+        $shampoo = new Product();
+        $shampoo->name = 'シャンプー';
+        echo $shampoo->name;
+        ?>
+    </p>
  
 
  </body>
